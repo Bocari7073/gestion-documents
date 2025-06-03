03534074e66940e745076481c43ada79a4d202d8
Bocari7073 <bocariguedeba@gmail.com>
2025-06-03 12:46:25 +0000

    Ajout components

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const isAuth = !!token;
+  const isLoginPage = req.nextUrl.pathname.startsWith('/login');
+
+  if (!isAuth && !isLoginPage) {
+    const loginUrl = req.nextUrl.clone();
+    loginUrl.pathname = '/login';
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*'],
+};
